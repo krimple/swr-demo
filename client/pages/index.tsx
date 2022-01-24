63fc59b35e1a4ac122288b1b4b64d199be2c5539
Ken Rimple <krimple@chariotsolutions.com>
2022-01-24 16:46:43 -0500

    more enhancements - add route nav with Link from next/link to /demo

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link';
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-
+      <Link href="/demo"><button>SWR demo</button></Link>
     </div>
   )
 }
--- client/shared/fetcher.ts
+++ client/shared/fetcher.ts
@@ -1,10 +1,21 @@
+/**
+ * This function provides a SWR fetching API and associated error handling
+ * for a set of RESTful URLs. It is configured using the <code>SWRConfig</code>
+ * component in the template <code>/client/pages/_app.tsx</code>.
+ *
+ * This example has a long-winded and sort of naive error handler that unpacks
+ * errors in payload formation as well as in > 200-level responses that are REST
+ * errors.
+ *
+ * @param resource the URL to fetch when using the fetcher
+ */
 export async function fetcher (resource: string) {
 // @ts-ignore
   let result;
   try {
     result = await fetch(resource);
   } catch (e) {
-    console.log('***** Problem with fetch that results in exception');
+    console.log('***** Problem with fetch that results in an exception');
     console.error(e);
     throw new Error('Invalid Response');
   }
